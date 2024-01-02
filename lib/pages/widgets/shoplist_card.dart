4499c2b8e899512d7378bb24931d936705bc54b3
Geraldo Quende <43283373+gquende@users.noreply.github.com>
2024-01-02 17:06:55 +0100

    FEAT: Remove some comments unecessary

--- lib/pages/widgets/shoplist_card.dart
+++ lib/pages/widgets/shoplist_card.dart
@@ -1,16 +1,9 @@
-
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 import 'package:ghouse/configs/theme.dart';
 
-// import 'package:provider/provider.dart';
-
 class ShoplistCard extends StatelessWidget {
-  // const BudgetCart({Key? key}) : super(key: key);
-
   ShoplistCard();
   @override
-
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
 
@@ -20,7 +13,11 @@ class ShoplistCard extends StatelessWidget {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height / 3.8,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8), color: Colors.white, boxShadow: [BoxShadow(color: Colors.black12,blurRadius: 7, spreadRadius: 1)]),
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(color: Colors.black12, blurRadius: 7, spreadRadius: 1)
+            ]),
         child: Padding(
           padding: const EdgeInsets.all(12.0),
           child: Column(
@@ -34,7 +31,10 @@ class ShoplistCard extends StatelessWidget {
                       Row(
                         children: [
                           CircleAvatar(
-                            child: Icon(Icons.kitchen, color: Colors.white,),
+                            child: Icon(
+                              Icons.kitchen,
+                              color: Colors.white,
+                            ),
                             backgroundColor: AppTheme.PRIMARYCOLOR,
                           ),
                           SizedBox(
@@ -66,9 +66,7 @@ class ShoplistCard extends StatelessWidget {
                         ],
                       ),
                       GestureDetector(
-                        onTap: () {
-
-                        },
+                        onTap: () {},
                         child: CircleAvatar(
                           child: Icon(
                             Icons.arrow_forward_ios_outlined,
@@ -151,8 +149,7 @@ class ShoplistCard extends StatelessWidget {
                             color: Color(0xff67727d).withOpacity(0.1)),
                       ),
                       Container(
-                        width: (size.width - 40) *
-                            (200 / 100),
+                        width: (size.width - 40) * (200 / 100),
                         height: 12,
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(50),
@@ -167,107 +164,5 @@ class ShoplistCard extends StatelessWidget {
         ),
       ),
     );
-
-    // return GestureDetector(
-    //   child: Consumer<WalletController>(builder: (_, _controller, child) {
-    //     return Padding(
-    //       padding: const EdgeInsets.only(bottom: 20),
-    //       child: Container(
-    //         width: double.infinity,
-    //         decoration: BoxDecoration(
-    //             color: Colors.white,
-    //             borderRadius: BorderRadius.circular(12),
-    //             boxShadow: [
-    //               BoxShadow(
-    //                 color: grey.withOpacity(0.01),
-    //                 spreadRadius: 10,
-    //                 blurRadius: 3,
-    //                 // changes position of shadow
-    //               ),
-    //             ]),
-    //         child: Padding(
-    //           padding:
-    //               EdgeInsets.only(left: 25, right: 25, bottom: 25, top: 25),
-    //           child: Column(
-    //             crossAxisAlignment: CrossAxisAlignment.start,
-    //             children: [
-    //               Text(
-    //                 budget.name,
-    //                 style: TextStyle(
-    //                     fontWeight: FontWeight.w500,
-    //                     fontSize: 13,
-    //                     color: Color(0xff67727d).withOpacity(0.6)),
-    //               ),
-    //               SizedBox(
-    //                 height: 10,
-    //               ),
-    //               Row(
-    //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //                 children: [
-    //                   Row(
-    //                     children: [
-    //                       Text(
-    //                         "${budget.amountConsume} Kzs",
-    //                         style: TextStyle(
-    //                           fontWeight: FontWeight.bold,
-    //                           fontSize: 20,
-    //                         ),
-    //                       ),
-    //                       SizedBox(
-    //                         width: 8,
-    //                       ),
-    //                       Padding(
-    //                         padding: const EdgeInsets.only(top: 3),
-    //                         child: Text(
-    //                           "${budget.percentComplete!.round()}%",
-    //                           style: TextStyle(
-    //                               fontWeight: FontWeight.w500,
-    //                               fontSize: 13,
-    //                               color: Color(0xff67727d).withOpacity(0.6)),
-    //                         ),
-    //                       ),
-    //                     ],
-    //                   ),
-    //                   Padding(
-    //                     padding: const EdgeInsets.only(top: 3),
-    //                     child: Text(
-    //                       "${budget.amount.round()} Kzs",
-    //                       style: TextStyle(
-    //                           fontWeight: FontWeight.w500,
-    //                           fontSize: 13,
-    //                           color: Color(0xff67727d).withOpacity(0.6)),
-    //                     ),
-    //                   ),
-    //                 ],
-    //               ),
-    //               SizedBox(
-    //                 height: 15,
-    //               ),
-    //               Stack(
-    //                 children: [
-    //                   Container(
-    //                     width: (size.width - 40),
-    //                     height: 4,
-    //                     decoration: BoxDecoration(
-    //                         borderRadius: BorderRadius.circular(5),
-    //                         color: Color(0xff67727d).withOpacity(0.1)),
-    //                   ),
-    //                   Container(
-    //                     width: (size.width - 40) *
-    //                         ((budget.percentComplete as double) / 100),
-    //                     height: 4,
-    //                     decoration: BoxDecoration(
-    //                         borderRadius: BorderRadius.circular(5),
-    //                         color: AppColors.primaryColor),
-    //                   ),
-    //                 ],
-    //               )
-    //             ],
-    //           ),
-    //         ),
-    //       ),
-    //     );
-    //   }),
-    // );
   }
 }
